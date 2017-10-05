6025e2396d6665c9f7df1e5805fd5adc3d7c2492
Ricardo Martinez <ricardo.martinez.monje@gmail.com>
2017-10-05 22:57:43 +0200

    2nd version

--- app.py
+++ app.py
@@ -1,23 +1,13 @@
-# import os
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello():
-#     return 'Hello World!'
-
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-#    if request.environ['HTTP_ORIGIN'] is not None:
-#        origin = request.environ['HTTP_ORIGIN']
-    origin = request.headers.get('HTTP_ORIGIN', '').lower()
+    headers = request.headers
+    #.get('HTTP_ORIGIN', '').lower()
 
-    return { "method" : request.method, "origin" : origin } 
+    return { "method" : request.method, "headers" : headers } 
 
 if __name__ == '__main__':
     app.run(debug = True, use_reloader = True)
