22cba5b07098dd42feb9ac6ed8c958d04f867cbe
Ricardo Martinez <ricardo.martinez.monje@gmail.com>
2017-10-05 23:26:28 +0200

    2nd version

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    origin = request.headers["Origin"]
+    origin = 'no-set'
+    origin = request.headers.get("Origin")
     return jsonify({ "method" : request.method, "origin" : origin })
 
 if __name__ == '__main__':
